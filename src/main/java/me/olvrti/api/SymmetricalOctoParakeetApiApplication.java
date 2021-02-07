d2b120212a610d5a7bf0af1f2ec178feb8cc7a77
Tiago Oliveira <olvrti@gmail.com>
2021-02-07 11:44:49 -0300

    chore: add spring boot

---
+++ src/main/java/me/olvrti/api/SymmetricalOctoParakeetApiApplication.java
@@ -0,0 +1,13 @@
+package me.olvrti.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymmetricalOctoParakeetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymmetricalOctoParakeetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/olvrti/api/SymmetricalOctoParakeetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.olvrti.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymmetricalOctoParakeetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
